8fd3cee69f0fb3afe35b9df278cc6a548f39b6c7
Jean Barcellos <jean.barcellos@hevo.com.br>
2019-10-30 17:40:03 -0300

    learning-props

---
+++ modulo01/learning-props/src/app.js
@@ -0,0 +1,17 @@
+'use strict'
+
+import React from 'react'
+import Title from './Title'
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className='container'>
+        <Title name='Jean' />
+      </div>
+    )
+  }
+}
+
+export default App
