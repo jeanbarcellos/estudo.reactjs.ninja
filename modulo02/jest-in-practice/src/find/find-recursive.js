3d6ca7aa2c497f6b92fdb456dabb2aafa424affd
Jean Barcellos <jean.barcellos@hevo.com.br>
2019-11-04 20:41:53 -0300

    Ajutes finos

--- modulo02/jest-in-practice/src/find/find-recursive.js
+++ modulo02/jest-in-practice/src/find/find-recursive.js
@@ -6,9 +6,9 @@ const find = (arr, func) => {
 
     return arrInternal.length === 0
       ? undefined
-      : func(head, counter, arr)
+      : (func(head, counter, arr)
         ? head
-        : findInternal(tail, counter + 1)
+        : findInternal(tail, counter + 1))
   })(arr, 0)
 }
 
