86ebb8e0ce1f01eaa47448028f20e91e8431ebf2
Jean Barcellos <jean.barcellos@hevo.com.br>
2019-11-10 19:04:31 -0300

    M2#A113 - Aplicação: editor de Markdown Criar arquivos MD

--- modulo02/applications/markdown-editor/src/components/markdown-editor/index.js
+++ modulo02/applications/markdown-editor/src/components/markdown-editor/index.js
@@ -1,15 +1,13 @@
 'use strict'
 
 import React, { PropTypes } from 'react'
-import Header from './markdown-editor-header'
+import Header from './header'
 
-const MarkdownEditor = ({ value, handleChange, handleRemove, isSaving, getMarkup }) => (
+const MarkdownEditor = ({ value, handleChange, getMarkup, textareaRef, ...props }) => (
   <section className='editor'>
-    <Header
-      isSaving={isSaving}
-      handleRemove={handleRemove} />
+    <Header {...props} />
 
-    <textarea value={value} onChange={handleChange} autoFocus />
+    <textarea value={value} onChange={handleChange} autoFocus ref={textareaRef} />
 
     <div className='view' dangerouslySetInnerHTML={getMarkup()} />
   </section>
@@ -18,7 +16,8 @@ const MarkdownEditor = ({ value, handleChange, handleRemove, isSaving, getMarkup
 MarkdownEditor.propTypes = {
   value: PropTypes.string.isRequired,
   handleChange: PropTypes.func.isRequired,
-  getMarkup: PropTypes.func.isRequired
+  getMarkup: PropTypes.func.isRequired,
+  textareaRef: PropTypes.func.isRequired
 }
 
 export default MarkdownEditor
