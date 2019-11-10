8cd1affdb033f130ae46afd7e84d8d4646e1353e
Jean Barcellos <jean.barcellos@hevo.com.br>
2019-11-10 17:35:49 -0300

    M2#A112 - Aplicação: editor de Markdown Removendo do localStorage

--- modulo02/applications/markdown-editor/src/components/markdown-editor.js
+++ modulo02/applications/markdown-editor/src/components/markdown-editor.js
@@ -3,9 +3,11 @@
 import React, { PropTypes } from 'react'
 import Header from './markdown-editor-header'
 
-const MarkdownEditor = ({ value, handleChange, isSaving, getMarkup }) => (
+const MarkdownEditor = ({ value, handleChange, handleRemove, isSaving, getMarkup }) => (
   <section className='editor'>
-    <Header isSaving={isSaving} />
+    <Header
+      isSaving={isSaving}
+      handleRemove={handleRemove} />
 
     <textarea value={value} onChange={handleChange} autoFocus />
 
@@ -16,7 +18,6 @@ const MarkdownEditor = ({ value, handleChange, isSaving, getMarkup }) => (
 MarkdownEditor.propTypes = {
   value: PropTypes.string.isRequired,
   handleChange: PropTypes.func.isRequired,
-  isSaving: PropTypes.bool.isRequired,
   getMarkup: PropTypes.func.isRequired
 }
 
