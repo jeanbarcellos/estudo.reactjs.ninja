78836aa308fc9a5f4a0c1a5d7f808a35ef4bcca8
Jean Barcellos <jean.barcellos@hevo.com.br>
2019-11-23 21:22:57 -0300

    M3#A19 – Adicionando mais contadores – testes – parte 2

--- modulo03/02-react-counter/src/redux-flow/reducers/counters/counters.test.js
+++ modulo03/02-react-counter/src/redux-flow/reducers/counters/counters.test.js
@@ -21,3 +21,17 @@ it('Should add a new counter again', () => {
   const after = [0, 1, 0]
   expect(counters(before, action)).to.be.deep.equal(after)
 })
+
+it('Should remove a counter', () => {
+  const before = deepFreeze([0, 1, 2])
+  const action = deepFreeze({ type: 'REMOVE_COUNTER', index: 1 })
+  const after = [0, 2]
+  expect(counters(before, action)).to.be.deep.equal(after)
+})
+
+it('Should remove a counter again', () => {
+  const before = deepFreeze([3, 1])
+  const action = deepFreeze({ type: 'REMOVE_COUNTER', index: 0 })
+  const after = [1]
+  expect(counters(before, action)).to.be.deep.equal(after)
+})
--- modulo03/02-react-counter/src/redux-flow/reducers/counters/index.js
+++ modulo03/02-react-counter/src/redux-flow/reducers/counters/index.js
@@ -1,8 +1,11 @@
 'use strict'
 
-export default (state , action) => {
+export default (state, action) => {
   switch (action.type) {
     case 'ADD_COUNTER':
       return state.concat(0)
+
+    case 'REMOVE_COUNTER':
+      return state.filter((_, index) => index !== action.index)
   }
 }
