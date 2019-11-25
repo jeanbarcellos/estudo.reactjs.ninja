3d61beb5f8f4ba5f0d0d21ba7ee2e69ad693d2cc
Jean Barcellos <jean.barcellos@hevo.com.br>
2019-11-25 15:57:09 -0300

    M3#A24-27 - App Todo List - Ajustes no boilerplate

---
+++ modulo03/03-todo-list/src/app.js
@@ -0,0 +1,44 @@
+'use strict'
+
+import React, { PureComponent } from 'react'
+
+// import Title from 'components/title'
+
+import './css/style.css'
+
+class App extends PureComponent {
+  constructor () {
+    super()
+    this.state = {
+      title: '...',
+      Component: 'div'
+    }
+  }
+
+  getTitle () {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('My app witth async / await!')
+      }, 3000)
+    })
+  }
+
+  async componentDidMount () {
+    const title = await import('components/title')
+
+    this.setState({
+      title: await this.getTitle(),
+      Component: title.default
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <this.state.Component>{this.state.title}</this.state.Component>
+      </div>
+    )
+  }
+}
+
+export default App
