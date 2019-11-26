6c95653efe4d9a9322c4f8e73556da8f8e21e598
Jean Barcellos <jean.barcellos@hevo.com.br>
2019-11-26 17:22:33 -0300

    M3#A53 - Busca CEP Populando tabela com dados do endereço

--- modulo03/04-todo-list-get-cep/src/components/search-cep/index.js
+++ modulo03/04-todo-list-get-cep/src/components/search-cep/index.js
@@ -5,17 +5,27 @@ import SearchCep from './search-cep'
 import ajax from '@fdaciuk/ajax'
 
 class SearchCepContainer extends PureComponent {
-  async componentDidMount () {
+  state = {
+    address: '',
+    city: '',
+    code: '',
+    district: '',
+    state: '',
+    status: 1
+  }
+
+  async componentDidMount() {
     const response = await ajax({
       headers: { 'content-type': null }
     }).get('http://apps.widenet.com.br/busca-cep/api/cep.json', {
       code: '88054-635'
     })
+    this.setState(response)
     console.log(response)
   }
 
-  render () {
-    return <SearchCep />
+  render() {
+    return <SearchCep {...this.state} />
   }
 }
 
