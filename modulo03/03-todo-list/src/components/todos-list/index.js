27b36a066fba425c8e46d4681fc3c72d03276a22
Jean Barcellos <jean.barcellos@hevo.com.br>
2019-11-26 15:17:13 -0300

    M3#A47 - App Todo List Mostrar todos filtrados

--- modulo03/03-todo-list/src/components/todos-list/index.js
+++ modulo03/03-todo-list/src/components/todos-list/index.js
@@ -3,10 +3,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { toggleTodo } from 'reducers/todos/action-creators'
+import * as filterActions from 'reducers/visibility-filter/actions'
 
-const TodosList = ({ todos, handleToggleTodo }) => (
+const TodosList = ({ todos, activeFilter, handleToggleTodo }) => (
   <ul>
-    {todos.map((todo) => (
+    {getVisibleTodos(todos, activeFilter).map(todo => (
       <li
         key={todo.id}
         style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
@@ -18,12 +19,21 @@ const TodosList = ({ todos, handleToggleTodo }) => (
   </ul>
 )
 
-const mapStateToProps = (state) => ({
-  todos: state.todos
+const getVisibleTodos = (todos, activeFilter) => {
+  return {
+    [filterActions.SHOW_ALL]: todos,
+    [filterActions.SHOW_COMPLETED]: todos.filter(todo => todo.completed),
+    [filterActions.SHOW_ACTIVE]: todos.filter(todo => !todo.completed)
+  }[activeFilter]
+}
+
+const mapStateToProps = state => ({
+  todos: state.todos,
+  activeFilter: state.visibilityFilter
 })
 
-const mapDispatchToProps = (dispatch) => ({
-  handleToggleTodo: (id) => (e) => {
+const mapDispatchToProps = dispatch => ({
+  handleToggleTodo: id => e => {
     dispatch(toggleTodo(id))
   }
 })
