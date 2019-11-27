52dd82b17f194d624730bbe909eaef890ecbb2ed
Jean Barcellos <jean.barcellos@hevo.com.br>
2019-11-27 14:53:29 -0300

    M3#A64 - Busca CEP Entendendo o poder dos middlewares

--- modulo03/04-todo-list-get-cep/src/index.js
+++ modulo03/04-todo-list-get-cep/src/index.js
@@ -5,27 +5,53 @@ import { render } from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
 
 // Redux
-import { createStore, applyMiddleware  } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 
 import App from './app'
 import reducer from 'reducers'
 
-const logger = ({ dispatch, getState }) => (next) => (action) => {
+// middleware - log
+const logger = ({ dispatch, getState }) => next => action => {
   console.log('LOGGER::will dispatch:', action)
   const nextAction = next(action)
   console.log('LOGGER::next action:', nextAction)
   return nextAction
 }
 
-const store = createStore(reducer, applyMiddleware(logger))
+// middleware - thunk
+const thunk = ({ dispatch, getState }) => next => action => {
+  if (typeof action === 'function') {
+    return action(dispatch, getState)
+  }
+  return next(action)
+}
+
+const store = createStore(reducer, applyMiddleware(logger, thunk))
+
+// -------------------
+store.dispatch(lazyAction())
+function lazyAction() {
+  return (dispatch, getState) => {
+    setTimeout(() => {
+      dispatch({
+        type: 'todos:ADD_TODO',
+        payload: {
+          text: 'Lazy Action',
+          id: '123'
+        }
+      })
+    }, 2000)
+  }
+}
+// -------------------
 
 const renderState = () => {
   console.log('state:', store.getState())
 }
 
 store.subscribe(renderState)
-renderState();
+renderState()
 
 /**
  * Rendenrizar o app
@@ -57,7 +83,6 @@ if (module.hot) {
     const nextReducer = require('reducers').default
     store.replaceReducer(nextReducer)
   })
-
 }
 
 // entrada de edição dos arquivos
