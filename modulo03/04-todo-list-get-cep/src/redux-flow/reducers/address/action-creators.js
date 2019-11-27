9d7f757c8f6ac1dbb7542576680ad1398a8b783d
Jean Barcellos <jean.barcellos@hevo.com.br>
2019-11-27 15:58:33 -0300

    M3#A67 - Busca CEP Ações assíncronas no app com Redux - isFetching no Redux

--- modulo03/04-todo-list-get-cep/src/redux-flow/reducers/address/action-creators.js
+++ modulo03/04-todo-list-get-cep/src/redux-flow/reducers/address/action-creators.js
@@ -1,16 +1,17 @@
 'use strict'
 
 import ajax from '@fdaciuk/ajax'
-import { UPDATE_ADDRESS } from './actions'
+import { FETCHING, SUCCESS } from './actions'
 
 export const fetchAddress = cep => async (dispatch, getState) => {
+  dispatch({ type: FETCHING })
+
   const response = await ajax({
     headers: { 'content-type': null }
   }).get('http://apps.widenet.com.br/busca-cep/api/cep.json', { code: cep })
-  dispatch(updateAddress(response))
-}
 
-export const updateAddress = data => ({
-  type: UPDATE_ADDRESS,
-  payload: data
-})
+  dispatch({
+    type: SUCCESS,
+    payload: response
+  })
+}
